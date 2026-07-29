7d7414b6541c80dfe1d9035cc7df1bc53f322b36
ISHANT SHARMA <159184246+ishantsharmapecpec@users.noreply.github.com>
2026-07-29 12:33:37 +0100

    Update tokenizer.py

--- search_engine/tokenizer.py
+++ search_engine/tokenizer.py
@@ -2,6 +2,12 @@ import re
 
 
 def tokenize(text: str) -> list[str]:
-   
+    """
+    Convert text into lowercase alphanumeric tokens.
 
+    Example:
+    "London Clay is stiff."
+    becomes:
+    ["london", "clay", "is", "stiff"]
+    """
     return re.findall(r"\b[a-zA-Z0-9]+\b", text.lower())
